5f15e021c380487e268cd2dbf2c8679dc701bd44
Krishvanth RS <122750632+krishvanthrs@users.noreply.github.com>
2023-02-16 19:34:30 +0000

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Class-18
+# PROC18-V3ColorfullTrex
+Colorful Trex
---
+++ array.js
@@ -0,0 +1,13 @@
+function setup() {
+     createCanvas(400, 400);
+    name = "Hermoine";
+    num = 31;
+    newArray = ["Item1", 32, name, num];
+    console.log(newArray[0]);
+    console.log(newArray[2]);
+    console.log(newArray.length);
+    } 
+    function draw() 
+    { 
+        background(220);
+     }
